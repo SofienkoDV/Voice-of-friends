f1b0c6e4404ae6821b49a95f41010988e08b546e
SofienkoDV <sofienko2933@gmail.com>
2023-03-15 16:57:03 +0200

    Added functionality to the registration form

--- src/modules/RegisterForm/components/RegisterForm.jsx
+++ src/modules/RegisterForm/components/RegisterForm.jsx
@@ -1,12 +1,19 @@
-import { LockOutlined, UserOutlined, MailOutlined } from "@ant-design/icons";
+import {
+  LockOutlined,
+  UserOutlined,
+  MailOutlined,
+  InfoCircleTwoTone,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 import { Form, Input } from "antd";
 import { Button, Block } from "../../../components";
-import { Link } from "react-router-dom";
 
 function RegisterForm() {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
   };
+
+  const success = false;
 
   return (
     <>
@@ -15,59 +22,76 @@ function RegisterForm() {
         <p>Будь ласка, зареєструйтесь в системі</p>
       </div>
       <Block>
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-        >
-          <Form.Item name="email">
-            <Input
-              prefix={<MailOutlined className="site-form-item-icon" />}
-              placeholder="Електронна пошта"
-              className="login-form__input"
-            />
-          </Form.Item>
+        {!success ? (
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+          >
+            <Form.Item name="email">
+              <Input
+                prefix={<MailOutlined className="site-form-item-icon" />}
+                placeholder="Електронна пошта"
+                className="login-form__input"
+              />
+            </Form.Item>
 
-          <Form.Item name="username">
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Ім'я користувача"
-              className="login-form__input"
-            />
-          </Form.Item>
-          <Form.Item validateStatus="success" hasFeedback name="password">
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Пароль"
-              className="login-form__input"
-            />
-          </Form.Item>
+            <Form.Item name="username">
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Ім'я користувача"
+                className="login-form__input"
+              />
+            </Form.Item>
+            <Form.Item validateStatus="success" hasFeedback name="password">
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Пароль"
+                className="login-form__input"
+              />
+            </Form.Item>
 
-          <Form.Item validateStatus="success" hasFeedback name="repeatPassword">
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Повторіть пароль"
-              className="login-form__input"
-            />
-          </Form.Item>
+            <Form.Item
+              validateStatus="success"
+              hasFeedback
+              name="repeatPassword"
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Повторіть пароль"
+                className="login-form__input"
+              />
+            </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" size="large">
-              Зареєструватися
-            </Button>
-          </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" size="large">
+                Зареєструватися
+              </Button>
+            </Form.Item>
 
-          <Form.Item>
-            <Link className="login-form__register-link" to="/">
-              Увійти в обліковий запис
-            </Link>
-          </Form.Item>
-        </Form>
+            <Form.Item>
+              <Link className="login-form__register-link" to="/">
+                Увійти в обліковий запис
+              </Link>
+            </Form.Item>
+          </Form>
+        ) : (
+          <div className="auth__success-block">
+            <div>
+              <InfoCircleTwoTone style={{ fontSize: "50px" }} />
+            </div>
+            <h2>Перевірте свою пошту</h2>
+            <p>
+              На вашу пошту надіслано листа з посиланням на підтвердження
+              облікового запису.
+            </p>
+          </div>
+        )}
       </Block>
     </>
   );
