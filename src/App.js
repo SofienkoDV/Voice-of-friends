c1087e47b0ed5dcac6be309de63a98946983f506
SofienkoDV <sofienko2933@gmail.com>
2023-03-24 00:29:43 +0200

    Updating node-sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Auth, Home } from "./pages";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="wrapper">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Link to="/register">Hello, world!</Link>} />
+        <Route path="/im" element={<Home />} />
         <Route path="*" element={<Auth />} />
       </Routes>
     </div>
